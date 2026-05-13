6498500e0d7586ac190bcfee96e647b2cb7d2dc9
KingChlorine <kingchloe281@gmail.com>
2026-05-13 22:42:08 +0100

    Added remove and quit option

--- GameLibrary.py
+++ GameLibrary.py
@@ -2,7 +2,7 @@ gamesInLibrary = []
 
 def gameLibrary():
     while True:
-        print("1.Open Game library 2. Add Game")
+        print("1.Open Game library 2. Add Game 3. Remove Game 4. Quit")
         main_menu_select = input("Type number to choose")
 
         if main_menu_select == "1":
@@ -11,5 +11,12 @@ def gameLibrary():
         if main_menu_select == "2":
             addGame = input("Type game title you wish to add:")
             gamesInLibrary.append(addGame)
-        continue
+
+        if main_menu_select == "3":
+            removeGame = input("Type game title you wish to remove:").lower
+            gamesInLibrary.remove(removeGame)
+
+        if main_menu_select == "4":
+            break
+
 gameLibrary()
