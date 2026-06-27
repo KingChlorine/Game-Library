287a07fe4c42ed76ff7b2c09e7138ebc10b212e2
KingChlorine <kingchloe281@gmail.com>
2026-06-27 10:55:55 +0100

    Removed Save option It was unnatural to remember to save manually. Instead, saving happens automatically after each addition and deletion.

--- GameLibrary.py
+++ GameLibrary.py
@@ -33,42 +33,23 @@ def saveToJson():
 
 def gameLibrary():
     while True:
-        print("1.Open Game library 2. Add Game 3. Remove Game 4. Quit 5. Save")
+        print("1.Open Game library 2. Add Game 3. Remove Game 4. Quit")
         main_menu_select = input("Type number to choose")
 
         if main_menu_select == "1":
-            #for item in gamesInLibrary:
-            #    print(item, sep= "\n")
             viewLibrary()
 
         if main_menu_select == "2":
-            #while True:
-            #    game = input("Type game title:")
-            #    platform = input("Type which platform:")
-            #    gamesInLibrary.append({"Game": game, "Platform": platform})
-                #games = json.dumps(gamesInLibrary, indent=4)
-                #with open("games.json", "w") as f:
-                #    f.write(games)
-            #    break
             addGame()
+            saveToJson()
 
         if main_menu_select == "3":
-           # removeGame = input("Type game title you wish to remove:")
-            #for game in gamesInLibrary:
-             #   if game["Game"].lower() == removeGame:
-              #      gamesInLibrary.remove(game)
               removeGame()
+              saveToJson()
 
         if main_menu_select == "4":
             print("GoodBye!")
             break
-
-        if main_menu_select == "5":
-            #games = json.dumps(gamesInLibrary, indent=4)
-            #with open("games.json", "w") as f:
-            #    f.write(games)
-            #    print("Library Saved")
-            saveToJson()
 
 
 gameLibrary()
